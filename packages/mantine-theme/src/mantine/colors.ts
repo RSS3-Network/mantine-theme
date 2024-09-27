1da6b867a1467faa7455b581fc1ba593f2d7c66a
Songkeys <i@song.work>
2024-09-27 23:40:02 +0800

    feat: update deps

---
+++ packages/mantine-theme/src/mantine/colors.ts
@@ -0,0 +1,16 @@
+import type { MantineThemeOverride } from '@mantine/core'
+
+export const colors: MantineThemeOverride['colors'] = {
+	blue: [
+		'#e5f4ff',
+		'#cde4ff',
+		'#9cc6fe',
+		'#67a7fc',
+		'#3c8cfb',
+		'#217cfa',
+		'#0d73fb',
+		'#0062e0',
+		'#0057c9',
+		'#004bb2',
+	],
+}
--- packages/mantine-theme/src/mantine/theme.ts
+++ packages/mantine-theme/src/mantine/theme.ts
@@ -1,5 +1,6 @@
 import { DEFAULT_THEME, type MantineTheme, createTheme } from '@mantine/core'
 import { deepmerge } from '../utils/deepmerge'
+import { colors } from './colors'
 // @ts-ignore
 import appShellClasses from './components/app-shell.module.css'
 // @ts-ignore
@@ -8,20 +9,7 @@ import buttonClasses from './components/button.module.css'
 import tooltipClasses from './components/tooltip.module.css'
 
 const themeOverride = createTheme({
-	colors: {
-		blue: [
-			'#e5f4ff',
-			'#cde4ff',
-			'#9cc6fe',
-			'#67a7fc',
-			'#3c8cfb',
-			'#217cfa',
-			'#0d73fb',
-			'#0062e0',
-			'#0057c9',
-			'#004bb2',
-		],
-	},
+	colors,
 	components: {
 		AppShell: {
 			classNames: appShellClasses,
